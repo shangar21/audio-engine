ca0c136d49f3abdba1290d446475a18fcf68073e
shangar <sharanshangar@gmail.com>
2025-06-06 13:45:42 -0400

    Some syntax fixes on the cmd

--- src/routes/sender.rs
+++ src/routes/sender.rs
@@ -1,7 +1,7 @@
+use crate::utils::run_command;
 use axum::extract::Json;
 use serde::Deserialize;
 use std::process::Command;
-use crate::utils::run_command;
 
 #[derive(Deserialize)]
 pub struct SendRequest {
@@ -13,7 +13,7 @@ pub struct SendRequest {
 
 pub async fn send(Json(payload): Json<SendRequest>) -> &'static str {
     let command = format!(
-        "gst-launch-1.0 {} device={} ! audio/x-raw.format=S16LE,channels=2,rate=44100 ! audioconvert ! rtpL16pay ! \"application/x-rtp,media=audio,encoding-name=L16,clock-rate=44100,payload=96\" ! udpsink host={} port={}",
+        "gst-launch-1.0 {} device={} ! audioconvert ! rtpL16pay ! \"application/x-rtp,media=audio,encoding-name=L16,clock-rate=44100,payload=96\" ! udpsink host={} port={}",
         payload.source, payload.device, payload.host, payload.port
     );
 
